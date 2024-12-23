8a6fa6c290f93ff01ca7fa0eee0631400e6e26ae
Louisejchi <a115744446@gmail.com>
2024-12-23 23:22:25 +0800

    modify id to autoincrement & reference pk's good_id to good's key

--- shopping/package_table.py
+++ shopping/package_table.py
@@ -6,7 +6,7 @@ def create_db():
   
   # Create Goods table
   c.execute('''CREATE TABLE IF NOT EXISTS Goods(
-      id VARCHAR(32) PRIMARY KEY, 
+      good_id INTEGER PRIMARY KEY AUTOINCREMENT, 
       name VARCHAR(128) NOT NULL,
       price INTEGER NOT NULL,
       img TEXT NOT NULL,
@@ -14,10 +14,13 @@ def create_db():
   
   # Create Package table
   c.execute('''CREATE TABLE IF NOT EXISTS Packages(
-      id VARCHAR(32) PRIMARY KEY,
+      package_id INTEGER PRIMARY KEY AUTOINCREMENT,
       place VARCHAR(128) NOT NULL,
       status VARCHAR(64) NOT NULL,
-      status_time DATETIME DEFAULT CURRENT_TIMESTAMP)''')
+      status_time DATETIME DEFAULT (datetime('now', 'localtime')),
+      good_id INTEGER,
+      FOREIGN KEY (good_id) REFERENCES Goods(good_id)
+  )''')
   conn.commit()
   conn.close()
 create_db()
