2c4a0d5a78934ac66e249d129403a5d1350f190c
Louisejchi <a115744446@gmail.com>
2024-12-24 20:08:22 +0800

    add new table -- Status & modify Packages

--- shopping/package_table.py
+++ shopping/package_table.py
@@ -15,12 +15,24 @@ def create_db():
   # Create Package table
   c.execute('''CREATE TABLE IF NOT EXISTS Packages(
       package_id INTEGER PRIMARY KEY AUTOINCREMENT,
-      place VARCHAR(128) NOT NULL,
-      status VARCHAR(64) NOT NULL,
-      status_time DATETIME DEFAULT (datetime('now', 'localtime')),
+      place VARCHAR(128),
+      -- place VARCHAR(128) NOT NULL,
+      -- status VARCHAR(64) NOT NULL,
+      -- status_time DATETIME DEFAULT (datetime('now', 'localtime')),
       good_id INTEGER,
       FOREIGN KEY (good_id) REFERENCES Goods(good_id)
   )''')
+
+  # FOR status
+  c.execute('''CREATE TABLE IF NOT EXISTS Status(
+      status_id INTEGER PRIMARY KEY AUTOINCREMENT,
+      status VARCHAR(64) NOT NULL,
+      status_time DATETIME DEFAULT (datetime('now', 'localtime')),
+      package_id INTEGER NOT NULL,
+      FOREIGN KEY (package_id) REFERENCES Packages(package_id)
+  )''')
+
+
   conn.commit()
   conn.close()
 create_db()
